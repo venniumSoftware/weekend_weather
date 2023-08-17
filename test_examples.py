8b5bcf9487eeb5a74fd191bef0cde1508b375d2a
Robert Fox Tross <robert.f.tross@vennium.com>
2023-08-17 18:53:13 -0400

    Update final, change name of workflow

--- test_examples.py
+++ test_examples.py
@@ -2,8 +2,16 @@
 
 import weekend_api
 import os
+import coverage
+
+# Start code coverage before importing other modules:
+cov = coverage.Coverage()
+cov.start()
 
 API_KEY = os.environ['API_KEY']
+
+import sys
+sys.path.append(sys.path[0] + "/..")
 
 def test_api():
     assert weekend_api.call_weather_api(API_KEY, 32259) != None
@@ -11,3 +19,8 @@ def test_api():
 if __name__ == "__main__":
     test_api()
     print("Everything Passed")
+    
+
+cov.stop()
+cov.save()
+cov.html_report(directory='coverage_reports')
