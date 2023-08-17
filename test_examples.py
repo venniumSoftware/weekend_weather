a737657cd05aba58dff7d2519aa77dc22ba4ac6c
Robert Fox Tross <robert.f.tross@vennium.com>
2023-08-17 18:24:50 -0400

    Update to fix unit tests

--- test_examples.py
+++ test_examples.py
@@ -2,5 +2,9 @@
 
 import weekend_api
 
-def test_uppercase():
+def test_api():
     assert weekend_api.call_weather_api() != None
+    
+if __name__ == "__main__":
+    test_api()
+    print("Everything Passed")
