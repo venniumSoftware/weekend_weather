74cb6c8771135fadf9ab6509b14f82f9cb8ed3d6
Robert Fox Tross <robert.f.tross@vennium.com>
2023-08-17 18:36:06 -0400

    Update to fix unit tests

--- test_examples.py
+++ test_examples.py
@@ -6,7 +6,7 @@ import os
 API_KEY = os.environ['API_KEY']
 
 def test_api():
-    assert weekend_api.call_weather_api() != None
+    assert weekend_api.call_weather_api(API_KEY, 32259) != None
     
 if __name__ == "__main__":
     test_api()
