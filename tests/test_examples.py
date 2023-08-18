36bdbf37ea03ae69338e3cea082a95ff29e2dfd7
Robert Fox Tross <robert.f.tross@vennium.com>
2023-08-17 22:34:06 -0400

    update

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -10,7 +10,7 @@ API_KEY = os.environ['API_KEY']
 def test_api():
     assert weekend_api.call_weather_api(API_KEY, 32259) != None
     
-if __name__ == "__main__":
+def main():
     test_api()
     print("Everything Passed")
     
