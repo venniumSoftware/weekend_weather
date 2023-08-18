73aa00695c9377f7142c0d3420700cafb219288d
Robert Fox Tross <robert.f.tross@vennium.com>
2023-08-18 02:05:00 -0400

    update

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,5 +1,6 @@
 # test_assert_examples.py
 import os, sys
+import pytest
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import weekend_api
@@ -7,6 +8,7 @@ import weekend_api
 
 API_KEY = os.environ['API_KEY']
 
+@pytest.fixture
 def test_api():
     assert weekend_api.call_weather_api(API_KEY, 32259) != None
     
