f2e7e0ccde631a45b43dc49f18e56347e2050a95
Robert Fox Tross <robert.f.tross@vennium.com>
2023-08-17 21:11:10 -0400

    update

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="weather_api",
+    version="0.0.1",
+    author="Robert Fox Tross",
+    author_email="robert.f.tross@vennium.com",
+    description="Package to create data ingestion",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
