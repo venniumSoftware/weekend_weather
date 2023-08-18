af7dc1698f9c74c12b51e03245b8c8269b58acc0
Robert Fox Tross <robert.f.tross@vennium.com>
2023-08-18 02:55:24 -0400

    update

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,13 +1,13 @@
 # test_assert_examples.py
 import os, sys
 import pytest
-from ..weekend_api import weekend_api
+from ..weekend_api.weekend_api import call_weather_api
 
 
 API_KEY = os.environ['API_KEY']
 
 def test_api():
-    assert weekend_api.call_weather_api(API_KEY, 32259) != None
+    assert call_weather_api(API_KEY, 32259) != None
     
 if __name__ == "__main__":
     test_api()
