37ca6642d22af29289e22da8f4c8f991ab7fa2fc
Robert Fox Tross <robert.f.tross@vennium.com>
2023-08-17 22:35:40 -0400

    update

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,7 +2,7 @@
 import os, sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import weekend_api
+import weather_api
 
 
 API_KEY = os.environ['API_KEY']
@@ -10,7 +10,7 @@ API_KEY = os.environ['API_KEY']
 def test_api():
     assert weekend_api.call_weather_api(API_KEY, 32259) != None
     
-def main():
+if __name__ == "__main__":
     test_api()
     print("Everything Passed")
     
